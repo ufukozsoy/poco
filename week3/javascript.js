4a9a73c673954f1c9bebdea6d31ad71d2324c264
ufukozsoy <ufuk.ozsoy@powercoders.org>
2024-09-30 22:28:39 +0200

    updated

--- week3/javascript.js
+++ week3/javascript.js
@@ -12,3 +12,16 @@ console.log(age)
 
 const pi=3.14
 console.log(pi)
+console.log(23+97)
+console.log((4+6+9)/77)
+
+let a=10
+console.log(a)
+console.log(9*a)
+let b=7*a
+console.log(b)
+
+let max=57
+let actual=max-13
+let percentage=actual/max
+console.log(percentage)
