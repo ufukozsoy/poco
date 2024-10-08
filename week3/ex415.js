603e862767b055b8f8334b96d32e8cc3a1d42353
ufukozsoy <ufuk.ozsoy@powercoders.org>
2024-10-08 08:13:42 +0200

    updated

--- week3/ex415.js
+++ week3/ex415.js
@@ -17,18 +17,64 @@ if (isNaN(num)){
  */
 
  //pop deletes the last element in the array
- let fruits=["apple","pear", "banana", "watermelon"]
+/*  let fruits=["apple","pear", "banana", "watermelon"]
  console.log(fruits.pop())
  console.log(fruits)
 
  //push append the element at the end of the array
  console.log(fruits.push("melon"))
- console.log(fruits)
+ console.log(fruits) */
 
-let firstName= "ufuk"
+/* let firstName= "ufuk"
 let lastName= "ozsyo"
 const fullname=firstName+ " "+lastName
-console.log(fullname)
+console.log(fullname) */
+
+/* let names=[]
+names[0]="name1"
+names[1]="name2"
+names[2]="name3"
+names[3]="name4"
+console.log(names)
+console.log(names.length)
+let name;
+for (name in names){
+    console.log()
+} */
+
+/* let arr=[0,1,2,3,4,5]
+function num(){
+    let sum=0
+
+    for(let i=0; i<arr.length; i++){
+
+        if(arr[i]%2===0){
+            sum=sum+arr[i]
+        }
+        
+    }
+    console.log(sum)
+
+}
+num() */
+
+/* let arr=[0,1,2,3,4,5,6]
+function num(){
+    let sum=0
+
+    for(item of arr){
+
+        if(item%2===0){
+            sum+=item
+        }
+        
+    }
+    console.log(sum)
+
+}
+num()
+ */
+
 
 
 
---
+++ week3/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+ let fruits=["apple","pear", "banana", "watermelon"]
+ console.log(fruits.pop())
+ console.log(fruits)
+
+ //push append the element at the end of the array
+ console.log(fruits.push("melon"))
+ console.log(fruits)
